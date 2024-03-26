c1f2729025b6e5160e363fdd8a0a31091e3aa074
Anna Goncharova <adn74@txstate.edu>
2024-03-26 14:01:37 -0500

    Added process class

--- discrete-simulator.py
+++ discrete-simulator.py
@@ -2,12 +2,21 @@ import random
 import math
 
 class Event:
+    def __init__():
 
 class Process:
+    def __init__(self, arrival_time, service_time):
+        self.arrival = arrival_time
+        self.service = service_time
+        self.start_time = None
+        self.finish_time = None
 
 class Event_Queue:
+    def __init__():
 
 def main():
+    #ave service time 
+    #for loop
 
 if __name__ == "__main__":
     main()
